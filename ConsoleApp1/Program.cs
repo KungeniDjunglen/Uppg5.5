0748970603ae7d731dc136e610cb70e9c6200b82
Hugge25 <hugo.paulsson@hotmail.com>
2023-01-20 10:25:16 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Brogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool gameOn = true;
+            string[] svar = { "Det blir så ibland", "Gud vet", "Det var Epstein som låg bakom det",
+                "Det finns många i världen som undrar samma sak", "Nummer 15, burgerking foot letuce", "JA", "NEJ", "Möjligtvis",
+                "Turkisk Pebbar", "Familjen Wallenberg" };
+            Random random = new Random();
+
+            Console.WriteLine("Välkommen til magic 8 ball, ställ din fråga och klotet kommer att svara på den. \nSkriv a när du är klar");
+            while (gameOn)
+            {
+                string question = Console.ReadLine();
+                if (question == "a")
+                {
+                    gameOn = false;
+                }
+                else
+                {
+                    Console.WriteLine(svar[random.Next(0, 10)]);
+                }
+            }
+        }
+    }
+}
